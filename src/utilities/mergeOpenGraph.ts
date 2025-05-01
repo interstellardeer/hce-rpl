989af75b53de241da9740477191ecd914315bd17
Arsya Permana@arsya <azelord18@gmail.com>
2025-05-01 14:37:50 +0700

    add OG image and favicon

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -6,7 +6,7 @@ const defaultOpenGraph: Metadata['openGraph'] = {
   description: 'An open-source website built with Payload and Next.js.',
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
+      url: `${getServerSideURL()}/logo-long-black-OG.webp`,
     },
   ],
   siteName: 'Human Centered Engineering - Rekayasa Perangkat Lunak',
