611663bfbd7d52526804e5f5eb1ce3aca3668096
Arsya Permana@arsya <azelord18@gmail.com>
2025-05-02 08:42:44 +0700

    fixing OG image

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -33,8 +33,8 @@ export default buildConfig({
         images: [
           {
             url: 'https://hce-rpl.vercel.app/logo-OG.webp',
-            width: 1280,
-            height: 720,
+            width: 1200,
+            height: 630,
           },
         ],
         siteName: 'HCE RPL',
